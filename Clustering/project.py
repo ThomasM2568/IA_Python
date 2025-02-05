7e39e1f8d2ec59b32049a1da39d582d5b524d4d3
Thomas Mirbey <101272724+ThomasM2568@users.noreply.github.com>
2025-02-05 09:01:09 +0100

    Update project.py

--- Clustering/project.py
+++ Clustering/project.py
@@ -1,3 +1,17 @@
+#!/usr/bin/python3
+'''
+Created on 05-02-2025
+
+@author: Pierre Meyer, Yohann Mitel, Kyllian Cuevas, Thomas Mirbey
+@version: 1
+
+Clustering project
+'''
+
+#------------------
+# Import
+#------------------
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -5,33 +19,39 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from ucimlrepo import fetch_ucirepo
 
-# Fetch dataset
-iris = fetch_ucirepo(id=53)
-X = iris.data.features
-y = iris.data.targets 
+#------------------
+# Main
+#------------------
 
-# Apply KMeans clustering
-kmeans = KMeans(n_clusters=3, init='random', random_state=42)
-clusters = kmeans.fit_predict(X)
 
-# Put together species by name
-species_mapping = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
-cluster_names = [species_mapping[label] for label in clusters]
-
-# Reduce dimensions using PCA
-pca = PCA(n_components=2)
-X_pca = pca.fit_transform(X)
-
-# Create a DataFrame for visualization
-df = pd.DataFrame(X_pca, columns=['PCA1', 'PCA2'])
-df['Species'] = cluster_names  # Use species names
-
-# Show clusters
-plt.figure(figsize=(8, 6))
-sns.scatterplot(x='PCA1', y='PCA2', hue=df['Species'], palette='Set1', data=df, s=100, edgecolor='k')
-plt.title("KMeans Clustering with Species Labels")
-plt.xlabel("Principal Component 1")
-plt.ylabel("Principal Component 2")
-plt.legend(title="Species")
-plt.show()
+if __name__ == '__main__':
+  # Fetch dataset
+  iris = fetch_ucirepo(id=53)
+  X = iris.data.features
+  y = iris.data.targets 
+  
+  # Apply KMeans clustering
+  kmeans = KMeans(n_clusters=3, init='random', random_state=42)
+  clusters = kmeans.fit_predict(X)
+  
+  # Put together species by name
+  species_mapping = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+  cluster_names = [species_mapping[label] for label in clusters]
+  
+  # Reduce dimensions using PCA
+  pca = PCA(n_components=2)
+  X_pca = pca.fit_transform(X)
+  
+  # Create a DataFrame for visualization
+  df = pd.DataFrame(X_pca, columns=['PCA1', 'PCA2'])
+  df['Species'] = cluster_names  # Use species names
+  
+  # Show clusters
+  plt.figure(figsize=(8, 6))
+  sns.scatterplot(x='PCA1', y='PCA2', hue=df['Species'], palette='Set1', data=df, s=100, edgecolor='k')
+  plt.title("KMeans Clustering with Species Labels")
+  plt.xlabel("Principal Component 1")
+  plt.ylabel("Principal Component 2")
+  plt.legend(title="Species")
+  plt.show()
 
